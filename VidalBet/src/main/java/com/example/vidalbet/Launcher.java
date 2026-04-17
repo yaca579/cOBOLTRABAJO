12eb92742fada3bbabf55d699988cdef739b8838
yaca579 <yaca579@vidalibarraquer.net>
2026-04-17 07:52:57 +0200

    Delete VidalBet directory

--- VidalBet/src/main/java/com/example/vidalbet/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.example.vidalbet;
-
-public class Launcher {
-    public static void main(String[] args) {
-        // Crida al mètode main de la classe Application
-        VidalBetApp.main(args);
-    }
-}
